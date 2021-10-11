076c76b698035c8e5b90559e676f46f898dbc1e0
Damini <81965482+gdamini@users.noreply.github.com>
2021-10-11 16:05:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+video="";
+status="";
+
+function setup(){
+    canvas=createCanvas(480, 380);
+    canavs.center();
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+image(video, 0, 0, 480, 380);
+}
+
+function start(){
+objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML="Status : Detecting Objects";    
+}
+
+function modelLoaded(){
+console.log("Model Loaded!");
+status=true;
+video.loop();
+video.speed(1);
+video.volume(0);
+}
